04df6f6901adca3cb48cdbf7ef686244b111fb89
OmarAlex24 <artcate208@gmail.com>
2024-10-08 10:30:09 -0600

    Tests unitarios terminados

--- src/test/java/com/omar/programa0/Programa0ApplicationTests.java
+++ src/test/java/com/omar/programa0/Programa0ApplicationTests.java
@@ -2,12 +2,38 @@ package com.omar.programa0;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ContextConfiguration(classes = Programa0Application.class)
 @SpringBootTest
 class Programa0ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void calcularTriangulo1(){
+        Triangulo triangulo = new Triangulo(5.0f, 12.0f);
+
+        assertEquals(30, triangulo.calcularArea());
+    }
+
+    @Test
+    void calcularTriangulo2(){
+        Triangulo triangulo = new Triangulo(3.0f, 0.0f);
+
+        assertEquals(0, triangulo.calcularArea());
+    }
+    @Test
+    void calcularTriangulo3(){
+        Triangulo triangulo = new Triangulo(0.0f, 3.0f);
+
+        assertEquals(0, triangulo.calcularArea());
+    }
+    @Test
+    void calcularTriangulo4(){
+        Triangulo triangulo = new Triangulo(5.0f, 5.0f);
+
+        assertEquals(12.5, triangulo.calcularArea());
     }
 
 }
