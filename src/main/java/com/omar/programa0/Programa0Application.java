71c2eeeb2aab50b3ad68b4fe5c48bc2fdb410880
OmarAlex24 <artcate208@gmail.com>
2024-10-08 09:58:56 -0600

    Proyect started

---
+++ src/main/java/com/omar/programa0/Programa0Application.java
@@ -0,0 +1,13 @@
+package com.omar.programa0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programa0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Programa0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/omar/programa0/Programa0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.programa0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programa0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
